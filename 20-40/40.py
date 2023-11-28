ab846659b96605c5b86fe85133d10ae139fce035
Yeongmin_Chae <codudals98@naver.com>
2023-11-29 01:43:23 +0900

    파일 이름 수정

--- 20-40/40.py
+++
@@ -1,9 +0,0 @@
-n, m = map(int, input().split())
-
-
-def func(n, m):
-    a = n*(m-1) + 1
-    return a
-
-
-print(func(n, m))
--- 40~60/47.py
+++
@@ -1,3 +0,0 @@
-K, N, M = map(int, input().split())
-
-print(K*N-M if K*N-M > 0 else 0)
