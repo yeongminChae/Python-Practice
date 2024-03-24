896f807b4bc408a34a037ddde4433ea7ff1583cd
채영민 <94206927+yeongminChae@users.noreply.github.com>
2024-03-25 00:26:22 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.26 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -2,8 +2,8 @@ def solution(my_string):
     answer = ''
     for i in my_string :
         if ord(i) >= 97 and ord(i) <= 123 :
-            answer += chr(ord(i) - 32)
+            answer += i.upper()
         elif ord(i) >= 65 and ord(i) <= 91 :
-            answer += chr(ord(i) + 32)
+            answer += i.lower()
             
     return answer
