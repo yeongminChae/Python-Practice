c02eab4f00d7fed67fb66c38cf1706ef0b54e980
채영민 <94206927+yeongminChae@users.noreply.github.com>
2024-03-24 23:53:44 +0900

    [level 0] Title: 모음 제거, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,9 +1,8 @@
 def solution(my_string):
     answer = ''
-    new_li = ['a','e','i','o','u']
     
     for i in my_string :
-        if i not in  new_li:
+        if i not in 'aeiou':
             answer += i
             
     return answer
