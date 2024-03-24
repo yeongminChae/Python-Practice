d08609399582374ddb89a86b0f29505dcf922dd4
채영민 <94206927+yeongminChae@users.noreply.github.com>
2024-03-25 00:27:29 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,9 +1,4 @@
 def solution(my_string):
-    answer = ''
-    for i in my_string :
-        if ord(i) >= 97 and ord(i) <= 123 :
-            answer += i.upper()
-        elif ord(i) >= 65 and ord(i) <= 91 :
-            answer += i.lower()
+    answer = my_string.swapcase()
             
     return answer
