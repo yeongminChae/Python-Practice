25360d5a6fc6b9d5246eab6ec79a226bb5539085
채영민 <94206927+yeongminChae@users.noreply.github.com>
2024-03-24 22:45:17 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,14 +1,4 @@
 def solution(my_string, num1, num2):
-    answer = ''
     new_li = list(map(str, my_string))
-    str1 = new_li[num1]
-    str2 = new_li[num2]
-    
-    for i, j in enumerate(new_li) :
-        if i == num1 :
-            j = str2
-        elif i == num2 :
-            j = str1
-        
-        answer += j
-    return answer
+    new_li[num1], new_li[num2] = new_li[num2], new_li[num1]
+    return ''.join(new_li)
