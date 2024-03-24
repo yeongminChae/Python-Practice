abda5a487d9b5d1fedd960c67d93c8d4a7dfc157
채영민 <94206927+yeongminChae@users.noreply.github.com>
2024-03-24 23:10:49 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.py
+++ 프로그래머스/0/120891. 369게임/369게임.py
@@ -1,11 +1,8 @@
 def solution(order):
     answer = 0
-    for i in list(map(str, str(order))) :
-        if i in '3' :
-            answer += 1
-        elif i in '6' :
-            answer += 1
-        elif i in '9' :
-            answer += 1
+    new_li = str(order)
+    
+    for i in '369' :
+        answer += new_li.count(i)
             
     return answer
