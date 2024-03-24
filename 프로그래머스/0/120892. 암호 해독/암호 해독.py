de551968e55f63cb407b0393ef5dad7cb3321466
채영민 <94206927+yeongminChae@users.noreply.github.com>
2024-03-24 22:59:42 +0900

    [level 0] Title: 암호 해독, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.py
@@ -1,9 +1,4 @@
 def solution(cipher, code):
-    answer = ''
-    new_li = list(map(str, cipher))
-    
-    for i, j in enumerate(cipher) :
-        if (i + 1) % code == 0 :
-            answer += j
+    answer = cipher[code - 1 :: code]
             
     return answer
