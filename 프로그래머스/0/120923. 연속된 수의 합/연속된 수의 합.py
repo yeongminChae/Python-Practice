dfba2cf8c6f2aea2142ec4398eb361549591b687
채영민 <94206927+yeongminChae@users.noreply.github.com>
2024-03-28 00:30:49 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,21 +1,2 @@
-def solution(num, total):
-    standard = (total * 2) // num
-    answer = [standard // 2]
-    
-    if num % 2 != 0 :    
-        a = 1
-        while len(answer) != num :
-            answer.append(standard // 2 - a)
-            answer.append(standard // 2 + a)
-            a += 1
-            
-    else :
-        n = standard - (standard // 2)
-        answer.append(n)
-        b = 1
-        while len(answer) != num :
-            answer.append(n + b)
-            answer.append(standard // 2 - b)
-            b += 1
-    
-    return sorted(answer)
+def solution(num, total):    
+    return [(total - (num * (num - 1) // 2)) // num + i for i in range(num)]
