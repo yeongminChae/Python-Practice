be0a7f343b5138c87477590083f82e13a18049d2
채영민 <94206927+yeongminChae@users.noreply.github.com>
2024-03-28 16:31:32 +0900

    [level 0] Title: 특이한 정렬, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
@@ -1,9 +1,4 @@
 def solution(numlist, n):
-    arr = [(i, abs(n - i)) for i in numlist]
-    sorted_arr = sorted(arr, key = lambda x: (x[1], -x[0]))
-    
-    answer = []
-    for i in sorted_arr :
-        answer.append(i[0])
+    sorted_arr = sorted(numlist, key = lambda x: (abs(n - x), -x))
         
-    return answer
+    return sorted_arr
