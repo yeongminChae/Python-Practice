222dc595c3504e0ccbd9000531b0af5887747248
채영민 <94206927+yeongminChae@users.noreply.github.com>
2024-04-01 22:35:55 +0900

    [level 0] Title: 접미사 배열, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
@@ -1,4 +1,3 @@
 def solution(my_string):
-    li = [my_string[::-1][0 : i] for i in range(1, len(my_string) + 1)]
-    answer = [i[::-1] for i in li]
+    answer = [my_string[i:] for i in range(len(my_string))]
     return sorted(answer)
