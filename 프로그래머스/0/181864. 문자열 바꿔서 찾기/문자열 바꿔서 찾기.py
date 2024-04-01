e68e4861534bade39bced493e771f9ba6c0ea859
채영민 <94206927+yeongminChae@users.noreply.github.com>
2024-04-01 23:33:31 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -1,5 +1,3 @@
 def solution(myString, pat):
-    a = ('').join([i.replace('A','-') for i in myString])
-    b = ('').join([i.replace('B','A') for i in a])
-    c = ('').join([i.replace('-','B') for i in b])
-    return 1 if pat in c else 0
+    a = myString.replace('A','-').replace('B','A').replace('-','B')
+    return 1 if pat in a else 0
