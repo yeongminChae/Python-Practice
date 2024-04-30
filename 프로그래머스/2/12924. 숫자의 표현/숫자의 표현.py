ade4368423e42ded576fc31cc37df30ef2b1e359
채영민 <94206927+yeongminChae@users.noreply.github.com>
2024-04-30 15:56:52 +0900

    [level 2] Title: 숫자의 표현, Time: 1.39 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,13 +1,8 @@
 def solution(n):
-    answer, temp = 0, 0
-    for i in range(1, n + 1) :
-        temp = 0
-        for j in range(i, n + 1) :
-            temp += j
-            if temp == n : 
-                answer += 1
-                break
-            elif temp > n :
-                break
-            
+    answer = 0
+# 등차수열: a = (n / k) - ((k - 1) * d) / 2, a = 초항, d = 공차, n = 주어진 합, k = 항의 갯수 
+# is_integer() 정수인지 확인하는 함수    
+    for k in range(1, n + 1) :
+        a = (n / k) - ((k - 1) * 1) / 2
+        if a.is_integer() and a > 0 : answer += 1
     return answer
